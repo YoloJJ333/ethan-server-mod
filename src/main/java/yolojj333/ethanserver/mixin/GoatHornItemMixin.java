691c6a24616c7591d114306bddfee307db473f87
YoloJJ333 <jaydenjjlee812@gmail.com>
2024-09-01 21:41:59 -0700

    goat horns no longer have cooldowns

---
+++ src/main/java/yolojj333/ethanserver/mixin/GoatHornItemMixin.java
@@ -0,0 +1,21 @@
+package yolojj333.ethanserver.mixin;
+
+import net.minecraft.entity.player.ItemCooldownManager;
+import net.minecraft.item.GoatHornItem;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GoatHornItem.class)
+public class GoatHornItemMixin {
+    @Redirect(
+            method = "use",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/ItemCooldownManager;set(Lnet/minecraft/item/Item;I)V"
+            )
+    )
+    private void removeHornCooldown(ItemCooldownManager instance, Item item, int duration) {
+    }
+}
--- src/main/java/yolojj333/ethanserver/mixin/enchantment/InfinityEnchantmentMixin.java
+++ src/main/java/yolojj333/ethanserver/mixin/enchantment/InfinityEnchantmentMixin.java
@@ -4,13 +4,11 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.enchantment.InfinityEnchantment;
 import net.minecraft.entity.EquipmentSlot;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Debug(export=true)
 @Mixin(InfinityEnchantment.class)
 public class InfinityEnchantmentMixin extends Enchantment {
     protected InfinityEnchantmentMixin(Rarity weight, EnchantmentTarget target, EquipmentSlot[] slotTypes) {
